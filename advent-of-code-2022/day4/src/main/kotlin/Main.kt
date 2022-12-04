a724b3353571baffb06d23e86103cb74b5feb4bb
Theo <theo.le.wiik@gmail.com>
2022-12-04 13:52:21 +0100

    Day four part one

--- advent-of-code-2022/day4/src/main/kotlin/Main.kt
+++ advent-of-code-2022/day4/src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-fun main(args: Array<String>) {
-  println("Hello World!")
+import java.io.File
 
-  // Try adding program arguments via Run/Debug configuration.
-  // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-  println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+  val path = getResourcePath("data.dat") ?: throw Exception("File does not exist")
+
+  var sum = 0
+  File(path).forEachLine {
+    val a = getMinMax(it.split(",").first())
+    val b = getMinMax(it.split(",").last())
+
+    if (isWithin(a, b) or isWithin(b, a)) sum++
+  }
+
+  println("Total: $sum")
 }
+
+fun isWithin(shouldBeWithin: Pair<Int, Int>, target: Pair<Int, Int>): Boolean =
+  shouldBeWithin.first >= target.first && shouldBeWithin.second <= target.second
+
+fun getResourcePath(path: String): String? =
+  object {}.javaClass.getResource(path)?.path
+
+fun getMinMax(str: String): Pair<Int, Int> =
+  Pair(str.split('-').first().toInt(), str.split('-').last().toInt())
