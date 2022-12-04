3461fe5d34992469d8fc6a8ff96df4d82eb89197
Theo <theo.le.wiik@gmail.com>
2022-12-04 14:02:12 +0100

    Day four part two

--- advent-of-code-2022/day4/src/main/kotlin/Main.kt
+++ advent-of-code-2022/day4/src/main/kotlin/Main.kt
@@ -3,18 +3,28 @@ import java.io.File
 fun main(args: Array<String>) {
   val path = getResourcePath("data.dat") ?: throw Exception("File does not exist")
 
-  var sum = 0
+  var fullyWithin = 0
+  var overlaps = 0
   File(path).forEachLine {
     val a = getMinMax(it.split(",").first())
     val b = getMinMax(it.split(",").last())
 
-    if (isWithin(a, b) or isWithin(b, a)) sum++
+    if (contains(a, b) or contains(b, a)) fullyWithin++
+    if (overlaps(a, b)) overlaps++
   }
 
-  println("Total: $sum")
+  println("Contains: $fullyWithin")
+  println("Overlaps: $overlaps")
 }
 
-fun isWithin(shouldBeWithin: Pair<Int, Int>, target: Pair<Int, Int>): Boolean =
+fun overlaps(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Boolean {
+  if (contains(Pair(p1.first, p1.first), p2) or contains(Pair(p2.first, p2.first), p1)) return true
+  if (contains(Pair(p1.second, p1.second), p2) or contains(Pair(p2.second, p2.second), p1)) return true
+
+  return false
+}
+
+fun contains(shouldBeWithin: Pair<Int, Int>, target: Pair<Int, Int>): Boolean =
   shouldBeWithin.first >= target.first && shouldBeWithin.second <= target.second
 
 fun getResourcePath(path: String): String? =
