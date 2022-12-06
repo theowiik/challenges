b4c4eec67374310cd2f8a47fc8e4a28fc44545ed
Theo <theo.le.wiik@gmail.com>
2022-12-06 10:45:18 +0100

    Day 6 refactor

--- advent-of-code-2022/day6/day6.js
+++ advent-of-code-2022/day6/day6.js
@@ -1,35 +1,24 @@
 const fs = require('fs');
+
+const daySix = (data, length) => {
+  let i = 0;
+
+  while (i < data.length) {
+    const subPart = data.substring(i, i + length).split('');
+
+    const hasDupe = subPart.some(
+      (c) => subPart.indexOf(c) !== subPart.lastIndexOf(c)
+    );
+
+    if (!hasDupe) {
+      return i + length;
+    }
+
+    i++;
+  }
+};
+
 const line = fs.readFileSync('data').toString();
 
-// "a bacd xxx" // 5
-// "0 1234 56789" // 1 + 4 = 5
-
-let i = 0;
-const finished = { partOne: false, partTwo: false };
-
-for (char in line) {
-  const startOfPacket = line.substring(i, i + 4).split('');
-  const startOfMessage = line.substring(i, i + 14).split('');
-
-  const hasStartOfPacket = startOfPacket.some(
-    (c) => startOfPacket.indexOf(c) !== startOfPacket.lastIndexOf(c)
-  );
-
-  const hasStartOfMessage = startOfMessage.some(
-    (c) => startOfMessage.indexOf(c) !== startOfMessage.lastIndexOf(c)
-  );
-
-  if (!hasStartOfPacket && !finished.partOne) {
-    console.log(`Part 1: ${i + 4}`);
-    finished.partOne = true;
-  }
-
-  if (!hasStartOfMessage && !finished.partTwo) {
-    console.log(`Part 2: ${i + 14}`);
-    finished.partTwo = true;
-  }
-
-  if (finished.partOne && finished.partTwo) break;
-
-  i++;
-}
+console.log(`Part 1: ${daySix(line, 4)}`);
+console.log(`Part 2: ${daySix(line, 14)}`);
