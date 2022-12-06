fcf944f7f8090e679d418f3b108681e1cb542109
Theo <theo.le.wiik@gmail.com>
2022-12-06 10:48:17 +0100

    Day 6 cleanup

--- advent-of-code-2022/day6/day6.js
+++ advent-of-code-2022/day6/day6.js
@@ -1,24 +1,17 @@
 const fs = require('fs');
+const line = fs.readFileSync('data').toString();
 
 const daySix = (data, length) => {
-  let i = 0;
-
-  while (i < data.length) {
+  for (let i = 0; i < data.length; i++) {
     const subPart = data.substring(i, i + length).split('');
 
     const hasDupe = subPart.some(
       (c) => subPart.indexOf(c) !== subPart.lastIndexOf(c)
     );
 
-    if (!hasDupe) {
-      return i + length;
-    }
-
-    i++;
+    if (!hasDupe) return i + length;
   }
 };
-
-const line = fs.readFileSync('data').toString();
 
 console.log(`Part 1: ${daySix(line, 4)}`);
 console.log(`Part 2: ${daySix(line, 14)}`);
